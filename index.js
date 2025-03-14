d9c88105e18f4a9a7f12a04976d17a5db8c97660
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-03-14 13:04:31 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.querySelector('#main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML ='Maxwell is the champion'
